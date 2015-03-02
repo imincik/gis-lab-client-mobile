edd8f1b1ccecd993c33c8df084c009cc6c13ca0b
Marcel Dancak <dancakm@gmail.com>
2015-03-02 09:01:01 +0100

    added server and project settings, used regular GET/POST ajax requests instead of JSONP

--- www/js/app.js
+++ www/js/app.js
@@ -3,8 +3,7 @@
 (function() {
 	'use strict';
 
-	var app = angular.module('app', ['onsen', 'ngTouch', 'ui.grid', 'ui.grid.edit', 'ivh.treeview']);
-
+	var app = angular.module('app', ['onsen', 'ngTouch', 'ngStorage', 'ui.grid', 'ui.grid.edit', 'ivh.treeview']);
 	app.config(function(ivhTreeviewOptionsProvider) {
 		ivhTreeviewOptionsProvider.set({
 			idAttribute: 'id',
@@ -22,13 +21,64 @@
 			//checkBoxTpl: '<p>x</p>'
 		});
 	});
-	app.controller('MapController', function($scope, $timeout) {
-		$scope.$parent.loadProject('natural-earth/central-europe');
+	app.config(['$httpProvider', function($httpProvider) {
+		//$httpProvider.defaults.useXDomain = true;
+		//delete $httpProvider.defaults.headers.common["X-Requested-With"];
+	}]);
+	app.controller('MapController', function($scope) {
+		console.log('MapController');
+		$scope.$parent.loadProject($scope.$storage.project);
 	});
 
-	app.controller('AppController', function($scope, $timeout, $http) {
+	app.controller('SettingsController', function($scope, $http) {
+		console.log('SettingsController');
+		var url = '{0}/projects.json'.format($scope.$storage.serverUrl);
+		$http.get(url, {withCredentials: true}).
+				success(function(data, status, headers, config) {
+					console.log(data);
+					if (data.length) {
+						$scope.myProjects.length = 0;
+						Array.prototype.push.apply($scope.myProjects, data);
+					}
+				}).
+				error(function(data, status, headers, config) {
+					console.log('error: '+status);
+				});
+	});
+
+	app.controller('AppController', function($scope, $http, $localStorage) {
+		$scope.mapInitialized = false;
 		console.log("AppController");
-		
+		$scope.settings = {
+			server_tab: 1,
+			project_tab: '1'
+		};
+		$scope.$storage = $localStorage;
+		$scope.myProjects = [];
+		if ($scope.$storage.serverUrl) {
+			$http.post('{0}/mobile/login/'.format($scope.$storage.serverUrl), {
+					username: $scope.$storage.username,
+					password: $scope.$storage.password
+				}, {
+					withCredentials: true
+				}).
+				success(function(data, status, headers, config) {
+					console.log('login successful');
+				}).
+				error(function(data, status, headers, config) {
+					console.log('login error');
+				});
+		}
+		ons.ready(function() {
+			console.log('ons ready');
+			$scope.app.navigator.on('postpop', function(evt) {
+				if (evt.enterPage.name == 'mainPage.html' && $scope.olMap.getSize()[0] == 0) {
+					console.log('update size');
+					$scope.olMap.updateSize();
+				}
+			});
+		});
+
 		$scope.mapWidth = document.body.clientWidth;
 		$scope.mapHeight = document.body.clientHeight;
 		$scope.panelHeight = document.body.clientHeight-45;
@@ -40,12 +90,12 @@
 		};
 
 		$scope.layers = {};
-		$scope.flat_layers = [];
+		$scope.layersList = [];
 
 		$scope.layersVisibilityChanged = function(node) {
 			var visible_layers = [];
-			$scope.flat_layers.forEach(function(layer_data) {
-				if (layer_data.visible) {
+			$scope.layersList.forEach(function(layer_data) {
+				if (!layer_data.isGroup && layer_data.visible) {
 					visible_layers.push(layer_data.name);
 				}
 			});
@@ -54,13 +104,17 @@
 		};
 
 		$scope.loadProject = function(project) {
-			var url = 'http://192.168.1.110:8877/mobile/config.json?PROJECT={project}&callback=JSON_CALLBACK'
+			console.log('loadProject '+project);
+			$scope.$storage.project = project;
+			var url = '{server}/mobile/config.json?PROJECT={project}'
+				.replace('{server}', $scope.$storage.serverUrl)
 				.replace('{project}', encodeURIComponent(project));
-			$http.jsonp(url).
+			$http.get(url).
 				success(function(data, status, headers, config) {
 					var controls = [
 						new ol.control.ButtonControl({
 							className: 'webgis-button panel',
+							html: '<span class="toolbar-button--quiet navigation-bar__line-height"><i class="ion-navicon" style="font-size:20px;"></i></span>',
 							callback: function(e) {
 								$scope.$apply(function() {
 									$scope.app.menu.toggleMenu();
@@ -69,10 +123,10 @@
 						}),
 						new ol.control.ButtonControl({
 							className: 'webgis-button menu',
+							html: '<span class="toolbar-button--quiet navigation-bar__line-height"><i class="ion-android-more" style="font-size:20px;"></i></span>',
 							callback: function(e) {
 								$scope.$apply(function() {
 									$scope.app.menu2.toggleMenu();
-									//$scope.loadProject('natural-earth/central-europe-base');
 								});
 							}
 						})
@@ -80,6 +134,22 @@
 					var webgis = angular.module('webgis');
 					var olMap = webgis.createMap(data);
 					if (olMap) {
+						$scope.mapInitialized = true;
+						if (!$scope.$storage.recentProjects) {
+							$scope.$storage.recentProjects = [data.project];
+						} else {
+							var index = $scope.$storage.recentProjects.indexOf(data.project);
+							if (index >= 0) {
+								$scope.$storage.recentProjects.splice(index, 1);
+							}
+							/*
+							while (index >= 0) {
+								$scope.$storage.recentProjects.splice(index, 1);
+								index = $scope.$storage.recentProjects.indexOf(data.project);
+								console.log('remove');
+							}*/
+							$scope.$storage.recentProjects.splice(0, 0, data.project);
+						}
 						if ($scope.olMap) {
 							$scope.olMap.dispose();
 						}
@@ -88,52 +158,28 @@
 						$scope.olMap.addControl(controls[1]);
 
 						$scope.layers = data.layers;
-						// create flat list of overlay layers
-						var visit_layer = function(layers, layer_data) {
-							layer_data.title = layer_data.title? layer_data.title : layer_data.name;
-							//layer_data.title += ' abcd efghij klmn';
-							if (layer_data.layers) {
-								var group_visible = true;
-								layer_data.layers.forEach(function(sublayer_data) {
-									visit_layer(layers, sublayer_data);
-									if (!sublayer_data.visible) {
-										group_visible = false;
-									}
-								});
-								layer_data.visible = group_visible;
-							} else if (layer_data) {
-								layers.push(layer_data);
+						$scope.layersList = webgis.layersTreeToList({layers: $scope.layers});
+						var attributions = {};
+						$scope.layersList.forEach(function(layer_data) {
+							var attribution = layer_data.attribution;
+							if (attribution) {
+								var attribution_html;
+								if (attribution.url) {
+									attribution_html = '<a href="{0}" target="_blank">{1}</a>'.format(attribution.url, attribution.title);
+								} else {
+									attribution_html = attribution.title;
+								}
+								attributions[layer_data.name] = new ol.Attribution({html: attribution_html});
 							}
-							return layers;
-						};
-						$scope.flat_layers = visit_layer([], {layers: $scope.layers});
+						});
+						$scope.olMap.getLayer("qgislayer").setLayersAttributions(attributions);
 						$scope.layersVisibilityChanged({});
 						var legends_urls = $scope.olMap.getLayer("qgislayer").getLegendUrls($scope.olMap.getView());
-						$scope.flat_layers.forEach(function(layer_data) {
+						$scope.layersList.forEach(function(layer_data) {
 							layer_data.legendUrl = legends_urls[layer_data.name];
 						});
 
-						var visit_baselayer = function(layers, layer_data, depth) {
-							if (!depth) {
-								depth = 0;
-							}
-							layer_data.depth = depth;
-							if (layer_data.layers) {
-								if (layer_data.title) {
-									layer_data.isGroup = true;
-									layers.push(layer_data);
-								}
-								layer_data.layers.forEach(function(sublayer_data) {
-									visit_baselayer(layers, sublayer_data, depth+1);
-								});
-							} else if (layer_data) {
-								layer_data.isGroup = false;
-								layers.push(layer_data);
-							}
-							return layers;
-						};
-						/*
-						data.base_layers = [
+						var test_base_layers = [
 							{title: 'First'},
 							{
 								title: 'Group',
@@ -150,8 +196,12 @@
 								]
 							},
 							{title: 'Third'},
-						];*/
-						$scope.baseLayers = visit_baselayer([], {layers: data.base_layers});
+						];
+						//$scope.baseLayersList = webgis.layersTreeToList({layers: test_base_layers});
+						$scope.baseLayersList = visit_baselayer([], {layers: data.base_layers});
+
+						// Project info
+						$scope.project = data;
 					}
 				}).
 				error(function(data, status, headers, config) {
@@ -170,6 +220,7 @@
 				$scope.mapWidth = document.body.clientWidth;
 				$scope.mapHeight = document.body.clientHeight;
 				$scope.$apply();
+				$scope.olMap.updateSize();
 			});
 			document.addEventListener("pause", onPause, false);
 			document.addEventListener("resume", function() {
@@ -225,5 +276,4 @@
 		}
 		];
 	});
-
 })();
