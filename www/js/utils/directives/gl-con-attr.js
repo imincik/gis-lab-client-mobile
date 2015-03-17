a94e7cd8972e181a8c28c85207bb1659016ecc99
Marcel Dancak <dancakm@gmail.com>
2015-03-17 09:45:44 +0100

    refatoring of 'gl.utils' module

--- www/js/utils/directives/gl-con-attr.js
+++ www/js/utils/directives/gl-con-attr.js
@@ -1,14 +1,21 @@
-angular.module('gl.utils', [])
-	.directive('glConAttr', function () {
+(function() {
+	'use strict';
+
+	angular
+		.module('gl.utils', [])
+		.directive('glConAttr', glConAttr);
+
+	function glConAttr() {
 		return {
 			restrict: 'A',
 			link: function (scope, el, attrs) {
 				var conditional_attributes = scope.$eval(attrs.glConAttr);
-				for (attr_name in conditional_attributes) {
+				for (var attr_name in conditional_attributes) {
 					if (conditional_attributes[attr_name] === true) {
 						attrs.$set(attr_name, '');
 					}
 				}
 			}
 		};
-	});
+	};
+})();
