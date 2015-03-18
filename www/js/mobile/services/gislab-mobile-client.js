de2681a8f0b77e7f0acfecab7f76c746a94b086e
Marcel Dancak <dancakm@gmail.com>
2015-03-18 07:22:02 +0100

    moved configuration of httpProvider into gislab-mobile-client.js

--- www/js/mobile/services/gislab-mobile-client.js
+++ www/js/mobile/services/gislab-mobile-client.js
@@ -3,6 +3,19 @@
 
 	angular
 		.module('gl.mobile')
+		.config(['$httpProvider', function($httpProvider) {
+			// Intercept POST requests, convert to standard form encoding
+			$httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8";
+			$httpProvider.defaults.transformRequest.unshift(function (data) {
+				var key, result = [];
+				for (key in data) {
+					if (data.hasOwnProperty(key)) {
+						result.push(encodeURIComponent(key) + "=" + encodeURIComponent(data[key]));
+					}
+				}
+				return result.join("&");
+			});
+		}])
 		.factory('gislabMobileClient', ['$http', gislabMobileClient]);
 
 	function gislabMobileClient($http) {
