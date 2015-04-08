89df6932540c752780eee28b9728298bd261c256
Marcel Dancak <dancakm@gmail.com>
2015-04-08 09:16:11 +0200

    refactoring of network client

--- www/js/mobile/services/gislab-mobile-client.js
+++ www/js/mobile/services/gislab-mobile-client.js
@@ -19,38 +19,56 @@
 		.factory('gislabMobileClient', ['$http', gislabMobileClient]);
 
 	function gislabMobileClient($http) {
-		return {
-			login: function(server, username, password) {
-				/*
-				return $http({
-					method: 'POST',
-					url: '{0}/mobile/login/'.format(server),
-					headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-					transformRequest: function(obj) {
-						var str = [];
-						for(var p in obj)
-						str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-						return str.join("&");
-					},
-					data: {username: username, password: password},
-				});*/
-				return $http.post('{0}/mobile/login/'.format(server), {
-						username: username,
-						password: password
-					}, {
-						withCredentials: true
-				});
-			},
-			project: function(server, project) {
-				return $http.get('{0}/mobile/config.json?PROJECT={1}'.format(server, encodeURIComponent(project)),{
-						withCredentials: true
-				});
-			},
-			userProjects: function(server) {
-				return $http.get('{0}/projects.json'.format(server), {
-						withCredentials: true
-				});
-			}
+		function GislabMobileClient() {};
+
+		GislabMobileClient.prototype.serverUrl = function(server) {
+			//var template = this._secure? 'https://{0}' : 'http://{0}';
+			var template = 'http://{0}';
+			return template.format(server);
 		};
+
+		GislabMobileClient.prototype.login = function(server, username, password) {
+			this._secure = true;
+			/*
+			return $http({
+				method: 'POST',
+				url: '{0}/mobile/login/'.format(this.serverUrl(server)),
+				headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+				transformRequest: function(obj) {
+					var str = [];
+					for(var p in obj)
+					str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+					return str.join("&");
+				},
+				data: {username: username, password: password},
+			});*/
+			return $http.post('{0}/mobile/login/'.format(this.serverUrl(server)), {
+					username: username,
+					password: password
+				}, {
+					withCredentials: true
+				}
+			);
+		};
+
+		GislabMobileClient.prototype.project = function(server, project) {
+			var url;
+			if (project && project !== 'empty') {
+				url = '{0}/mobile/config.json?PROJECT={1}'.format(this.serverUrl(server), encodeURIComponent(project));
+			} else {
+				url = '{0}/mobile/config.json?'.format(this.serverUrl(server));
+			}
+			return $http.get(url, {
+					withCredentials: true
+			});
+		};
+
+		GislabMobileClient.prototype.userProjects = function(server) {
+			return $http.get('{0}/projects.json'.format(this.serverUrl(server)), {
+					withCredentials: true
+			});
+		};
+
+		return new GislabMobileClient();
 	};
 })();
